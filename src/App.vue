cca057d4ef6d500d0f9b69c7ede198fff69949f2
KYOTA KONNO <konno@kyotar.com>
2019-05-26 12:09:07 +0900

    スタイル調整

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style>
+<script>
+</script>
+
+<style lang="scss">
+@import '@/assets/css/common.scss';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,59 +1,113 @@
 <template>
   <div class="home">
+    <header class="header">
+      <h1>graffiti</h1>
+    </header>
 
-    <template v-for="(item, index) in comments">
-      {{item}}
-      <p><img :src="item.avatar" alt=""></p>
-      <p>{{item.content}}</p>
-    </template>
+    <div v-for="(item, index) in comments" :data-id="item.id" class="comments">
+      <p class="content circle-1">{{ item.content }}</p>
+      <time class="time">{{ item.createdAt.toDate().toLocaleString() }}</time>
+    </div>
 
-    <input type="text">
-    <button type="button">送信</button>
-    <!-- <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <footer class="footer">
+      <textarea class="textarea" v-model="inputComment" maxlength="31" rows="5" placeholder="なにを刻む？" required autofocus></textarea>
+      <button type="button" class="button" :disabled="!inputComment" @click="add">送信</button>
+    </footer>
   </div>
 </template>
 
 <script>
-// import HelloWorld from '@/components/HelloWorld.vue'
 import db from '@/plugins/firebase'
-
-
-
-// db.collection("comments").get().then(function(querySnapshot) {
-//     querySnapshot.forEach(function(doc) {
-//         // doc.data() is never undefined for query doc snapshots
-//         console.log(doc.id, " => ", doc.data());
-//     });
-// });
-
 
 export default {
   name: 'home',
   data() {
     return {
+      inputComment: '',
       comments: [],
     }
   },
-  // data: () => ({
-  //   comments: [],
-  // }),
 
-  created() {
-    // db.collection('comments').add({ content: 'test'})
-    // db.collection('comments').doc(id).delete()
-  },
-  //
-  // components: {
-  //   HelloWorld
-  // },
   firestore() {
      return {
        comments: db.collection('comments').orderBy('createdAt')
      }
    },
+
+  created() {
+    // db.collection('comments').add({ content: 'test'})
+    // db.collection('comments').doc(id).delete()
+  },
+
+  methods: {
+    add() {
+      const now = new Date()
+
+      db.collection('comments').add({
+        content: this.inputComment,
+        createdAt: now
+      }).then((docRef) => {
+        this.inputComment = ''
+        console.log("Document written with ID: ", docRef.id)
+      })
+      .catch((error) => {
+        console.error("Error adding document: ", error)
+      });
+    }
+  },
 }
 </script>
 
-<style lang="sass">
-  </style>
+<style lang="scss" scoped>
+@import '@/assets/css/common.scss';
+.header {
+  padding: 16px;
+  background-color: $color-primary;
+
+  h1 {
+    margin: 0;
+    font-size: $font-l;
+    text-align: center;
+  }
+}
+
+.home {
+  // text-align: center;
+}
+
+.comments {
+  margin: 16px auto;
+    width: 240px;
+
+  .content {
+    background-color: rgba($color-black, .1);
+    padding: 32px;
+    margin: 0;
+
+    min-height: 32px;
+    border-radius: 4px;
+    font-size: $font-m;
+  }
+
+  .circle-1 {
+    border-radius: 50% 50% 50% 70%/50% 50% 70% 60%;
+  }
+  .circle-2 {
+    border-radius: 80% 30% 50% 50%/50%;
+  }
+  .circle-3 {
+    border-radius: 40% 40% 50% 40%/30% 50% 50% 50%;
+  }
+
+  .time {
+    font-size: $font-xs;
+    color: rgba($color-black, .4);
+  }
+}
+
+.footer {
+  text-align: center;
+  display: fixed;
+  bottom: 0;
+}
+</style>
