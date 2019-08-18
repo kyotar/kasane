7f54ac52bee793c752f88c02ffe9e0597ccdff4a
KYOTA KONNO <konno@kyotar.com>
2019-08-18 13:12:28 +0900

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,10 +4,16 @@
       <h1>graffiti</h1>
     </header>
 
+    <div>
+      <p>方角（geo）{{geo}}</p>
+      <p>方角（デバイス）{{dev}}</p>
+    </div>
+
     <div v-for="(item, index) in comments" :data-id="item.id" class="comments">
       <p class="content circle-1">{{ item.content }}</p>
-      <time class="time">{{ item.timestamp.toDate().toLocaleString() }}</time>
-      <!-- <div class="location">{{ item.location }}</div> -->
+      <time class="time" v-if="item.timestamp">{{ item.timestamp.toDate().toLocaleString() }}</time>
+      <div class="location" v-if="item.timestamp">{{ item.location._lat }} - {{ item.location._long }}</div>
+      <div class="location" v-if="item.head">{{ item.head }}</div>
     </div>
 
     <footer class="footer">
@@ -30,7 +36,12 @@ export default {
       geo: {
         lat: '', // 緯度
         long: '', // 経度
-      }
+        head: '', // 方角
+      },
+      dev: {
+        compass: null,
+        acc: null,
+      },
     }
   },
 
@@ -42,37 +53,82 @@ export default {
 
   created() {
     this.getGeo()
+
+    window.addEventListener('deviceorientation', (event) => {
+      console.log('方角       : ' + event.alpha);
+      console.log('上下の傾き : ' + event.beta);
+      console.log('左右の傾き : ' + event.gamma);
+      this.dev.compass = event.webkitCompassHeading
+      this.dev.acc = event.webkitCompassAccuracy
+      console.log('コンパスの向き : ' + event.webkitCompassHeading);
+      console.log('コンパスの精度 : ' + event.webkitCompassAccuracy);
+    }, true)
   },
 
   methods: {
+    /**
+    * コメント追加
+    */
     addComment() {
-      console.log(this.geo.lat)
+      this.getGeo() // 投稿前に位置情報をセットし直す
+
       db.collection('comments').add({
         content: this.inputComment,
         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        heading: this.geo.head,
         location: new firebase.firestore.GeoPoint(this.geo.lat, this.geo.long),
       }).then((docRef) => {
         this.resetComment()
         console.log('Document written with ID: ', docRef.id)
       })
       .catch((error) => {
-        console.error('Error adding document: ', error)
+        console.warn(error)
       })
     },
 
+    /**
+    * コメント欄をリセット
+    */
     resetComment() {
       this.inputComment = ''
     },
 
+
+    /**
+    * 位置情報を取得
+    */
     getGeo() {
-      const vm = this
+      // const vm = this
 
       navigator.geolocation.getCurrentPosition((position) => {
-        vm.geo.lat = position.coords.latitude
-        vm.geo.long = position.coords.longitude
+        this.geo.lat = position.coords.latitude
+        this.geo.long = position.coords.longitude
+        this.geo.head = position.coords.heading
       }),
+
+      // 取得エラー
       (error) => {
-        console.error('位置情報取得失敗', error)
+        switch(error.code) {
+          case 1: //PERMISSION_DENIED
+            alert('位置情報の利用が許可されていません')
+            break
+          case 2: //POSITION_UNAVAILABLE
+            alert('現在位置が取得できませんでした')
+            break
+          case 3: //TIMEOUT
+            alert('タイムアウトになりました')
+            break
+          default:
+            alert('Other Error', error.code)
+            break
+        }
+      },
+
+      // オプション
+      {
+        'enableHighAccuracy': true,
+        'timeout': 10000,
+        'maximumAge': 0
       }
     }
   },
@@ -121,6 +177,11 @@ export default {
   }
 
   .time {
+    font-size: $font-xs;
+    color: rgba($color-black, .4);
+  }
+
+  .location {
     font-size: $font-xs;
     color: rgba($color-black, .4);
   }
